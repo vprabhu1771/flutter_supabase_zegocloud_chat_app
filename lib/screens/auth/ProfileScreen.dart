47678bac3b7741a1fea1547b189ff6c319f8de40
vprabhu1771 <vprabhu1771@gmail.com>
2025-04-20 17:12:21 +0530

    ProfileScreen EditProfileScreen added

--- lib/screens/auth/ProfileScreen.dart
+++ lib/screens/auth/ProfileScreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../widgets/CustomDrawer.dart';
 import '../HomeScreen.dart';
+import 'EditProfileScreen.dart';
 
 class ProfileScreen extends StatefulWidget {
   final String title;
@@ -14,7 +15,14 @@ class ProfileScreen extends StatefulWidget {
 
 class _ProfileScreenState extends State<ProfileScreen> {
 
-  final user = supabase.auth.currentUser;
+  var user = supabase.auth.currentUser;
+
+  Future<void> refreshUserData() async {
+    await supabase.auth.refreshSession();
+    setState(() {
+      user = supabase.auth.currentUser; // Update user state
+    });
+  }
 
   Future<void> signOut() async {
     await supabase.auth.signOut();
@@ -56,8 +64,18 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 leading: Icon(Icons.person),
                 title: Text(user?.userMetadata?['name']), // Replace with dynamic user name
                 trailing: Icon(Icons.edit),
-                onTap: () {
+                onTap: () async {
                   // Handle the edit profile action
+                  bool? result = await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => EditProfileScreen(),
+                    ),
+                  );
+
+                  if (result == true) {
+                    await refreshUserData(); // Refresh data after edit
+                  }
                 },
               ),
               const Divider(),
