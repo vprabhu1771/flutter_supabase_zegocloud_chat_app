5cdebfa4c8b41ff9fbadb60f32d547fd3b454f59
vprabhu1771 <vprabhu1771@gmail.com>
2025-04-20 17:06:59 +0530

    Create HomeScreen.dart

---
+++ lib/screens/HomeScreen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../widgets/CustomDrawer.dart';
+
+class HomeScreen extends StatefulWidget {
+
+  final String title;
+
+  const HomeScreen({super.key, required this.title});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      drawer: CustomDrawer(parentContext: context),
+      body: Center(
+        child: Text(widget.title),
+      ),
+    );
+  }
+}
