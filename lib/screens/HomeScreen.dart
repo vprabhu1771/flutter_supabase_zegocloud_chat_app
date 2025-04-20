e87622d04f18f2578a4fca29515662c88501f804
vprabhu1771 <vprabhu1771@gmail.com>
2025-04-20 18:25:23 +0530

    floatingActionButton added to Home Screen

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_supabase_zegocloud_chat_app/screens/NewChatScreen.dart';
 import '../widgets/CustomDrawer.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -21,6 +22,16 @@ class _HomeScreenState extends State<HomeScreen> {
       drawer: CustomDrawer(parentContext: context),
       body: Center(
         child: Text(widget.title),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => NewChatScreen(title: 'New Chat')),
+          );
+        },
+        child: const Icon(Icons.add_circle),
+        tooltip: 'View Users',
       ),
     );
   }
